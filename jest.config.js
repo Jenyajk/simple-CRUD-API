315a0a5e3773a87a63e64d3d2cdcb9b6ef6303dd
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2023-05-15 23:50:49 +0500

    feat: add test

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
